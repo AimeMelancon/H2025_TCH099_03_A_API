58d7dc9b0ed78cd50538bece6f9e0e95ff34218e
AimeMelancon <aimemelancon@gmail.com>
2025-03-11 21:16:46 -0400

    getModule fonctionnel pour la base actually

--- api/v1/modules/getModule.py
+++ api/v1/modules/getModule.py
@@ -6,14 +6,16 @@ from app import db
 def getModule(module):
     #Création de la requête à la db
     stmt =(
-        Select(Module.nom, Module.schema)
+        Select(Module)
         .filter(Module.nom ==module)
     )
     #Permet d'avoir le résultat de la requête SQLite
     result = db.session.execute(stmt).scalars().first()
-     
-    if  result :
-         return jsonify({"nom": result.nom, "schema": result.schema}),200
+    print(result)
+    #Traitement du résultat 
+    if result:
+       #TODO:Si on peut on va faire en sorte que c'est la requête qui se charge de sélectionner les paramètres souhaités!
+        return jsonify({"nom": result.nom, "schema": result.schema}), 200
     else:
         return jsonify({"error": "Module  non trouvé"}),404
     
