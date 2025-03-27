f33e3f3229f7514e090e34f98e13c77e7d319f0e
AimeMelancon <aimemelancon@gmail.com>
2025-03-27 17:59:39 -0400

    correction mineure

--- api/v1/utilisateurs/getAdmin.py
+++ api/v1/utilisateurs/getAdmin.py
@@ -5,7 +5,7 @@ import datetime
 from sqlalchemy import Select
 from flask_bcrypt import check_password_hash
 import jwt
-
+import sys
 
 def coAdmin(pseudo,mdp):
     """Permet de se connecter en temps qu'administrateur
@@ -21,16 +21,17 @@ def coAdmin(pseudo,mdp):
             .filter(Utilisateur.pseudo == pseudo)
             .filter(Utilisateur.admin == 1))
          
-         result =db.session.execute(stmt).first()
+         result =db.session.execute(stmt).scalars().first()
          #Permet de vérifier si l'identifiant est valide
          if result:
              
              # Encodage du mot de passe de l'utilisateur
+            print(result, file=sys.stderr)
             connect = check_password_hash(result.mdp,mdp)
              #Permet de vérifier la connexion avec le mot de passe
             if connect:
                  #Génère un token
-                 token = jwt.encode({'pseudo': pseudo, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15)},app.config['secret_key'])
+                 token = jwt.encode({'pseudo': pseudo, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15)},app.config['secret_key'],algorithm="HS256")
                  #Renvoie la réponse à la requête positive
                  return jsonify({"Connexion" : True, "Utilisateur": pseudo , "token": token }),200
             else:
@@ -38,4 +39,4 @@ def coAdmin(pseudo,mdp):
          else:
              return jsonify({"error":"L'identifiant ou le mot de passe fournie n'est pas valide."}),401
     except Exception as e:
-        return jsonify({"error":"Échec de la connexion veuillez réessayer"}),404
+        return jsonify({"error":f"Échec de la connexion veuillez réessayer {e}"}),404
