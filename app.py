15a4b2a9e5621886ba66cdb8e750031fd2545df9
Rémi Cloutier <remicloutier2005@gmail.com>
2025-04-13 20:15:38 -0400

    load dotenv

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os 
 
 db = SQLAlchemy()
 
 def create_app():
     """Initialise le framework Flask. Retourne l'application flask."""
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+
+    # Load la clé secrète du fichier .env
+    secret_key = os.getenv("SECRET_KEY")
+
     app = Flask(__name__)
     CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
-    app.config['secret_key'] = "C'est un secret"
+    app.config['secret_key'] = secret_key
     
     
     app.json.sort_keys = False # Retourne les json en ordre de la base de données, pas alphabétique
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 from app import create_app, db
 flask_app = create_app()
+import os
+from dotenv import load_dotenv
+
+debug_mode = os.getenv("DEBUG")
 
 
 # Crée les tables (seulement si elles n'existent pas déjà)
 with flask_app.app_context():
     db.create_all()
 
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+
+# Savoir à partir du .env si on est en debug ou pas
+debug_mode = os.getenv("DEBUG", "False").lower() == "true"
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=5000, debug=False)
+    flask_app.run(host='0.0.0.0', port=5000, debug=debug_mode)
